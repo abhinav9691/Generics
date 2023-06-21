281ebbbbe1eb82bf8e013f12460aeed33e77cc1d
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-21 17:49:59 +0530

    [Abhinav]-adding Int Part

---
+++ IntComparison.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class IntComparison<T> where T : IComparable
+    {
+        public T Comparevalues(T a, T b, T c)
+        {
+            if(a.CompareTo(b)>0 && a.CompareTo(c) > 0)
+            {
+                return a;
+            }
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                return b;
+            }
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
+            {
+                return c;
+            }
+            return default;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿
+using Generics;
+
+IntComparison<int> i = new IntComparison<int>();
+Console.WriteLine(i.Comparevalues(4, 5, 6));
